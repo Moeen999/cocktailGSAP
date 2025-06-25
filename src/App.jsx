cf44672c6a4d619cf96beb4238d0242016c57fb7
Muhammad Moeen <mooenmalik374@gmail.com>
2025-06-25 01:46:27 -0700

    Implemented the cocktails section animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { gsap, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -8,8 +9,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className=" h-dvh bg-black"></div>
-      <div className=" h-dvh bg-black"></div>
+      <Cocktails/>
+    
     </main>
   );
 };
